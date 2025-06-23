f1acb61c5df6ca745b1513caaf5d4a61926e4cee
JoelPech <franciscopechtun712@gmail.com>
2025-06-23 12:31:03 -0600

    Barra de Busqueda

--- src/components/TarjetaBook.tsx
+++ src/components/TarjetaBook.tsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router';
 import './TarjetaBook.css';
 
 interface TarjetaBookProps {
-  propietario: React.ReactNode; // Cambiado a ReactNode para permitir un botón o texto
-  propietarioImagen: string; // Opcional si quieres mostrar una imagen del propietario
+  propietario: React.ReactNode;
+  propietarioImagen: string;
   titulo: string;
   imagen: string;
   id: string;
@@ -26,24 +26,25 @@ const TarjetaBook: React.FC<TarjetaBookProps> = ({
   publication_year,
   onVerMas,
 }) => {
-
   const navigate = useNavigate();
 
   return (
     <div className="tarjeta-book">
-      <div className="propietario-info" onClick={() => navigate('/perfil-usuario')} title="Ver perfil del propietario">
+      <div
+        className="propietario-info"
+        onClick={() => navigate('/perfil-usuario')}
+        title="Ver perfil del propietario"
+      >
         <img
           className="propietario-img"
           src={propietarioImagen}
-          alt={titulo}
+          alt="Propietario"
         />
         <span className="propietario-nombre">{propietario}</span>
       </div>
+
       <h3 className="tarjeta-book-titulo">{titulo}</h3>
       <img className="tarjeta-book-imagen" src={imagen} alt={titulo} />
-      {/* <button className="tarjeta-book-btn" onClick={onIntercambiar}>
-        Intercambiar
-      </button> */}
 
       <button
         className="tarjeta-book-btn"
@@ -62,12 +63,12 @@ const TarjetaBook: React.FC<TarjetaBookProps> = ({
       >
         Intercambiar
       </button>
+
       <button className="tarjeta-book-link" onClick={onVerMas}>
-        Ver mas
+        Ver más
       </button>
     </div>
   );
 };
-
 
 export default TarjetaBook;
